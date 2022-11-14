7147b5362fc97340a9edd856f28697c7ac5e9daf
AsadoDeCordero <j.ignaciorebolledo@gmail.com>
2022-11-14 11:25:50 -0300

    hola

--- resources/views/panel/resultados.blade.php
+++ resources/views/panel/resultados.blade.php
@@ -17,133 +17,22 @@
                 </div>
             </div>
         </div>
-        <!-- breadcrumb end -->
-
-
         <div class="latest-result">
             <div class="container">
                 <div class="row">
-                <!--
-                    <div class="col-xl-6 col-lg-6">
-                        <div class="single-match">
-                            <div class="part-head">
-                                <h5 class="match-title">BBPL 2019 Semi Final</h5>
-                                <span class="match-venue">Venue : Sher-e-Bangla National Stadium. Mirpur, Dhaka</span>
-                            </div>
-                            <div class="part-team">
-                                <div class="single-team">
-                                    <div class="logo">
-                                        <img src="assets/img/team-1.png" alt="">
-                                    </div>
-                                    <span class="team-name">Khulna Tigers</span>
-                                </div>
-                                <div class="match-details">
-                                    <div class="match-time">
-                                        <span class="date">Fri 09 Oct 2019 || 09:00 am</span>
-                                    </div>
-                                    <div class="goal">
-                                        <ul>
-                                            <li>2</li>
-                                            <li>3</li>
-                                        </ul>
-                                        <span class="text">full time</span>
-                                    </div>
-                                </div>
-                                <div class="single-team win-team">
-                                    <div class="logo">
-                                        <span class="win">win</span>
-                                        <img src="assets/img/team-2.png" alt="">
-                                    </div>
-                                    <span class="team-name">Dhaka Platoon</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xl-6 col-lg-6">
-                        <div class="single-match">
-                            <div class="part-head">
-                                <h5 class="match-title">BBPL 2019 Semi Final</h5>
-                                <span class="match-venue">Venue : Sher-e-Bangla National Stadium. Mirpur, Dhaka</span>
-                            </div>
-                            <div class="part-team">
-                                <div class="single-team win-team">
-                                    <div class="logo">
-                                        <span class="win">win</span>
-                                        <img src="assets/img/team-3.png" alt="">
-                                    </div>
-                                    <span class="team-name">Khulna Tigers</span>
-                                </div>
-                                <div class="match-details">
-                                    <div class="match-time">
-                                        <span class="date">Fri 09 Oct 2019 || 09:00 am</span>
-                                    </div>
-                                    <div class="goal">
-                                        <ul>
-                                            <li>2</li>
-                                            <li>3</li>
-                                        </ul>
-                                        <span class="text">full time</span>
-                                    </div>
-                                </div>
-                                <div class="single-team">
-                                    <div class="logo">
-                                        <img src="assets/img/team-4.png" alt="">
-                                    </div>
-                                    <span class="team-name">Dhaka Platoon</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div> 
-                    -->
-                     <div class="col-xl-6 col-lg-6" id="divpartido->partido_id">
-                        <div class="single-match">
-                            <div class="part-head">
-                                <h5 class="match-title">partido->titulo</h5>
-                            </div>
-                            <div class="part-team">
-                                <input type="hidden" name="partido_id" id="partidopartido->partido_id" value="partido->partido_id">
-                                <div class="single-team win-team">
-                                    <div class="logo">
-                                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Flag_of_Germany.svg/1920px-Flag_of_Germany.svg.png" alt="">
-                                    </div>
-                                    <span class="team-name">partido->local</span>
-                                </div>
-                                <div class="match-details">
-                                    <div class="match-time">
-                                        <span class="date">partido->fecha_completa</span>
-                                    </div>
-                                    <div class="goal">
-                                        <ul>
-                                            <li>1</li>
-                                            <li>2</li>
-                                        </ul>
-                                        <span class="text">partido->estado</span>
-                                    </div>
-                                </div>
-                                <div class="single-team">
-                                    <div class="logo">
-                                        <span class="win">Ganador</span>
-                                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Flag_of_Australia_%28converted%29.svg/1920px-Flag_of_Australia_%28converted%29.svg.png" alt="">
-                                    </div>
-                                    <span class="team-name">partido->visita</span>
-                                </div>
-                            </div>
-                            <div class="col-md-12" style="text-align:center;padding: 0px;">
-                                <a id="boton_partido->partido_id" onclick="return null;" class="btn btn-danger" style="background: #dc3545;cursor: default;width:100%;color:white"> Tu apuesta <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Flag_of_Germany.svg/1920px-Flag_of_Germany.svg.png" style="height: 15px;"> 0 - 0 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Flag_of_Australia_%28converted%29.svg/1920px-Flag_of_Australia_%28converted%29.svg.png" style="height: 15px;"></a>
-                            </div>
-                        </div>
-                    </div>
                     @foreach($partidos as $partido)
-                    <!--
+                    @if($partido->estadopartido_id==3)
                     <div class="col-xl-6 col-lg-6" id="div{{$partido->partido_id}}">
                         <div class="single-match">
                             <div class="part-head">
                                 <h5 class="match-title">{{$partido->titulo}}</h5>
                             </div>
                             <div class="part-team">
-                                <input type="hidden" name="partido_id" id="partido{{$partido->partido_id}}" value="{{$partido->partido_id}}">
                                 <div class="single-team win-team">
                                     <div class="logo">
+                                      @if($partido->res_local_real>$partido->res_visita_real)
+                                      <span class="win">Ganador</span>
+                                      @endif
                                         <img src="{{$partido->logo_local}}" alt="">
                                     </div>
                                     <span class="team-name">{{$partido->local}}</span>
@@ -154,25 +43,37 @@
                                     </div>
                                     <div class="goal">
                                         <ul>
-                                            <li><input type="text" class=" textoPrediccion form-control" id="res_local{{$partido->partido_id}}" @if($partido->pronostico==1) value="{{$partido->res_local}}"@endif></li>
-                                            <li><input type="text" class=" textoPrediccion form-control" id="res_visita{{$partido->partido_id}}" @if($partido->pronostico==1) value="{{$partido->res_visita}}"@endif></li>
+                                            <li>{{$partido->res_local_real}}</li>
+                                            <li>{{$partido->res_visita_real}}</li>
                                         </ul>
                                         <span class="text">{{$partido->estado}}</span>
                                     </div>
                                 </div>
                                 <div class="single-team">
                                     <div class="logo">
+                                      @if($partido->res_visita_real>$partido->res_local_real)
+                                      <span class="win">Ganador</span>
+                                      @endif
                                         <img src="{{$partido->logo_visita}}" alt="">
                                     </div>
                                     <span class="team-name">{{$partido->visita}}</span>
                                 </div>
                             </div>
                             <div class="col-md-12" style="text-align:center;padding: 0px;">
-                                <a id="boton_{{$partido->partido_id}}" onclick="enviar('{{$partido->partido_id}}')" class="btn @if($partido->pronostico==1) btn-warning @else btn-success @endif" style="width:100%;color:white"> @if($partido->pronostico==1) Modificar pronóstico @else Enviar pronóstico @endif</a>
+                                <a id="boton_partido->partido_id" onclick="return null;" 
+                                @if($partido->puntos==0)
+                                class="btn btn-danger" 
+                                @elseif($partido->puntos==1)
+                                class="btn btn-warning"
+                                @elseif($partido->puntos==3)
+                                class="btn btn-success"
+                                @endif
+                                style="cursor: default;width:100%;color:white"
+                                > Tu apuesta <img src="{{$partido->logo_local}}" style="height: 15px;"> {{$partido->res_local}} - {{$partido->res_visita}} <img src="{{$partido->logo_visita}}" style="height: 15px;"></a>
                             </div>
                         </div>
                     </div>
-                    -->
+                    @endif
                     @endforeach
                 </div>
             </div>
